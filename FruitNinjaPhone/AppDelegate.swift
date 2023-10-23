167f29a1e114977f159dee8d50280e653874434b
rongwei <rongwei.ji@outlook.com>
2023-10-23 14:07:49 -0500

    Initial Commit

---
+++ FruitNinjaPhone/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  FruitNinjaPhone
+//
+//  Created by RongWei Ji on 10/23/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ FruitNinjaPhone/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FruitNinjaPhone
+//
+//  Created by RongWei Ji on 10/23/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
