47ae5541a6dfae270fdc6f83642704528b76f9ef
rongwei <rongwei.ji@outlook.com>
2023-10-23 14:18:28 -0500

    create the scene class file

---
+++ FruitNinjaPhone/FruitGameScene.swift
@@ -0,0 +1,16 @@
+//
+//  FruitGameScene.swift
+//  FruitNinjaPhone
+//
+//  Created by RongWei Ji on 10/23/23.
+//
+
+import Foundation
+import SpriteKit
+import CoreMotion
+import AVFoundation
+
+class FruitGameScene:SKScene, SKPhysicsContactDelegate{
+    
+    
+}
